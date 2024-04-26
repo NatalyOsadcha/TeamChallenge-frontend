f51ceef1c75e96eb6e756315f4e28d297ff2a2b0
NatalyOsadcha <114231206+NatalyOsadcha@users.noreply.github.com>
2024-04-27 02:22:02 +0300

    added logic to add products to cart

---
+++ src/products.js
@@ -0,0 +1,23 @@
+const products = [
+  {
+    id: 1,
+    productName: 'grape',
+    productPrice: 12,
+  },
+  {
+    id: 2,
+    productName: 'raspberry',
+    productPrice: 25,
+  },
+  {
+    id: 3,
+    productName: 'greens',
+    productPrice: 35,
+  },
+  {
+    id: 4,
+    productName: 'onions',
+    productPrice: 2,
+  },
+];
+export default products;
